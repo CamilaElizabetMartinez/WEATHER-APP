66280c7d85101b4849a5525aaff175111beb79c1
camila martinez <martinezce_@hotmail.com>
2021-09-25 16:11:56 +0300

    refactor(Weather): importamos iconos y agregamos parametro state

--- weather-app/src/components/Weather/Weather.jsx
+++ weather-app/src/components/Weather/Weather.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
-import { WiCloud } from "react-icons/wi";
+import { WiCloud,WiDayCloudy,WiDayFog,WiDaySunny,WiRain} from "react-icons/wi";
 import {IconContext} from 'react-icons'
 
-const Weather = ({ temperature }) => {
+const Weather = ({ temperature, state}) => {
+  const stateByName = {
+    cloud: WiCloud,
+    cloudy: WiDayCloudy,
+    fog: WiDayFog,
+    sunny: WiDaySunny,
+    rain: WiRain 
+  }
+
+  const renderState = state => {
+    const 
+  }
   return (
     <div>
       <IconContext.Provider value={{size:'5em'}}>
       <WiCloud></WiCloud>
       </IconContext.Provider>
       
-      <Typography display="inline" variant="h2">{temperature} </Typography>
+      <Typography display="inline" variant="h3">{temperature} </Typography>
     </div>
   );
 };
