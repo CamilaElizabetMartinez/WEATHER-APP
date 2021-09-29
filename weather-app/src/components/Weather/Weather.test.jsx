fb4a2dfb297842a12522602d3513305f944ec126
camila martinez <martinezce_@hotmail.com>
2021-09-29 18:10:33 +0300

    refactor(component/Weather/Weather.test): modificamos el nombre del test

--- weather-app/src/components/Weather/Weather.test.jsx
+++ weather-app/src/components/Weather/Weather.test.jsx
@@ -4,7 +4,7 @@ import {render} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 //TDD
-test("Weather render", async () => {
+test("Weather render sunny", async () => {
 
     //AAA Arrange Act Assert
 
