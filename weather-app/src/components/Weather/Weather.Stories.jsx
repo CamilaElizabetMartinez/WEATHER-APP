448edaf54225ccfcfd6f087bc051cb6195990842
camila martinez <martinezce_@hotmail.com>
2021-09-29 18:03:46 +0300

    refactor(components/Weather/Weather.Stories): generamos ejemplos para ver las variantes del componente Weather, añadimos propiedad state y le agregamos un valor por defecto.

--- weather-app/src/components/Weather/Weather.Stories.jsx
+++ weather-app/src/components/Weather/Weather.Stories.jsx
@@ -6,4 +6,5 @@ export default {
     component: Weather
 }
 
-export const WeatherExample = () => <Weather temperature="10"></Weather>
+export const WeatherCloud = () => <Weather temperature="10" state="cloud"></Weather>
+export const WeatherSunny = () => <Weather temperature="10" state="sunny"></Weather>
