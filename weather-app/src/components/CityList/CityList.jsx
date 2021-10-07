c04a296b207bb032342d62d214ecd645a2cd2459
camila martinez <martinezce_@hotmail.com>
2021-10-07 15:12:22 +0300

    feat(src/components/CityList): creamos componente con parametro y lo validamos

---
+++ weather-app/src/components/CityList/CityList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CityList = ({cities}) => {
+    return (
+        <div>
+            CityList
+        </div>
+    )
+}
+
+CityList.propTypes = {
+    city: PropTypes.array.isRequired,
+
+}
+
+export default CityList
