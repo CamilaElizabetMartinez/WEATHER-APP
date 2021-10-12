0f8eba192ef7a9bb3b3274259ad7383f1f1b3965
camila martinez <martinezce_@hotmail.com>
2021-10-12 12:45:03 +0300

    refactor(src/components/CityList):  importamos componente weather, retornamos  dentro de tag li los componentes CityInfo y Weather

--- weather-app/src/components/CityList/CityList.jsx
+++ weather-app/src/components/CityList/CityList.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import CityInfo from '../CityInfo/CityInfo'
+import React from "react";
+import PropTypes from "prop-types";
+import CityInfo from "../CityInfo/CityInfo";
+import Weather from "../Weather/Weather";
 
-const renderCityAndCountry = cityAndCountry => {
-  const {city,country} = cityAndCountry
-  
-}
+const renderCityAndCountry = (cityAndCountry) => {
+  //li: es un item (segun tag html) 
+  const { city, country } = cityAndCountry;
+  return (
+    <li>
+      <CityInfo city={city} country={country} />
+      <Weather temperature={10}/>
+    </li>
+  );
+};
+
+//cities: es un array, y en cada item tiene que tener city y el country.
+//ul: tag html para listas no ordenadas
 
 const CityList = ({ cities }) => {
-  return <ul>{}</ul>;
+  return (
+    <ul>
+      {cities.map((cityAndContry) => renderCityAndCountry(cityAndContry))}
+    </ul>
+  );
 };
 
 CityList.propTypes = {
