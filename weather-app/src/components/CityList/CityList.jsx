b8cc739edb300bacecf9df03c9dab356239408df
camila martinez <martinezce_@hotmail.com>
2021-10-12 12:21:47 +0300

    refactor(src/components/CityList):  importamos CityInfo,creamos renderCityAndCountry y le pasamos el parametro cityAndCountry

--- weather-app/src/components/CityList/CityList.jsx
+++ weather-app/src/components/CityList/CityList.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
+import CityInfo from '../CityInfo/CityInfo'
+
+const renderCityAndCountry = cityAndCountry => {
+   
+}
 
 const CityList = ({ cities }) => {
   return <ul>{}</ul>;
