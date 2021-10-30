fd39edaa879f4a00c0c2c381c41d3e0b454f2edb
camila martinez <martinezce_@hotmail.com>
2021-10-30 20:02:11 +0300

    refactor(src/components/CityList): creamos array cities

--- weather-app/src/components/CityList/CityList.test.jsx
+++ weather-app/src/components/CityList/CityList.test.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import {render} from '@testing-library/react'
 import CityList from './CityList'
+const cities = [
+    {city:"Buenos Aires", country:"Argentina"},
+    {city:"Bogotá", country:"Colombia"},
+    {city:"París",  country:"Francia"},
+    {city:"Málaga", country:"España"},
+]
 test("CityList renders", async() => {
     
 })
